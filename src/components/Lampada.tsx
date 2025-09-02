1e7e96591008f2c7586e8d9a6767c915bbf6c1d2
Andressa <contatoandressaskm@gmail.com>
2025-09-01 21:30:53 -0300

    att atv lampada

--- src/components/Lampada.tsx
+++ src/components/Lampada.tsx
@@ -12,11 +12,11 @@ function Lampada() {
   return (
     <div className='flex flex-col items-center justify-center h-52 w-52'>
       <img className=''
-        src={ligada ? 'lampada_acesa.png' : 'lampada_apagada.png'}
+        src={ligada ? 'lampada_acesa (2).png' : 'lampada_apagada.png'}
         alt={ligada ? 'Lâmpada acesa' : 'Lâmpada apagada'}
       />
-      <div className='flex flex-col items-center'>
-        <button className='bg-blue-700 w-32 h-10 text-white rounded-4xl' onClick={alternarLampada}>
+      <div className='flex flex-col items-center rounded-4xl'>
+        <button className={ ligada ? 'toggle-button-on bg-gray-800 text-white h-8 w-32 rounded-4xl' : 'toggle-button-off bg-green-400 text-white h-8 w-32 rounded-4xl'}onClick={alternarLampada}> 
         {ligada ? 'Desligar' : 'Ligar'}
       </button>
       </div>
